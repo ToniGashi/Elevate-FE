5ca8e64dc2143ba2060e9bf917fa4de9e32718ed
Martin Mitev <martin.mitev@sciant.com>
2022-01-18 00:14:28 +0200

    [GF-22] Started Specific Project Implementation

---
+++ src/components/cards/ProjectCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-card class="bg-grey-2 q-pa-xl" flat>
+    <q-card-section class="text-center q-mb-lg">
+      <div class="text-h2 text-weight-bold q-mb-sm">{{currentProject.name}}</div>
+      <div class="text-subtitle1 text-weight-medium text-muted text-italic">
+        {{currentProject.motto}}
+      </div>
+    </q-card-section>
+
+    <q-card-section horizontal>
+      <q-img
+        class="col-lg-9 col-sm-8"
+        :src="currentProject.img"/>
+
+      <q-card-actions vertical>
+        <standard-button :size="$q.screen.lg ? '1.7rem': 'md'"
+                         padding="sm xl"
+                         unelevated
+                         color="secondary"
+                         label-class="text-weight-bold text-no-wrap"
+                         :label="`Invest in ${currentProject.name}`"/>
+      </q-card-actions>
+
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import StandardButton from 'components/buttons/StandardButton'
+export default {
+  name: 'ProjectCard',
+  props: ['currentProject'],
+  components: { StandardButton }
+}
+</script>
+
+<style lang="sass" scoped>
+.q-card
+  width: 100%
+</style>
--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,12 +1,15 @@
 <template>
   <q-page padding>
+    <ProjectCard :current-project="currentObject"/>
   </q-page>
 </template>
 
 <script>
+import ProjectCard from 'components/cards/ProjectCard'
 import childFetcher from 'components/mixins/childFetcher.js'
 export default {
   name: 'Project',
-  mixins: [childFetcher]
+  mixins: [childFetcher],
+  components: { ProjectCard }
 }
 </script>
