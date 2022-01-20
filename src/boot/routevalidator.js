ce4a805da0fb6a23284567665e07e5ba32ad6571
Martin Mitev <martin.mitev@sciant.com>
2022-01-21 01:03:46 +0200

    [GF-40] Integrated Project Marketplace Into Landing Page

--- src/boot/routevalidator.js
+++ src/boot/routevalidator.js
@@ -15,11 +15,11 @@ export default boot(({ router, store, redirect }) => {
     } else {
       store.dispatch(`${pathSubstr}/get${upperCasePath}`)
         .then(() => {
-          data = store.getters[`${pathSubstr}/get${upperCasePath}`]
+          data = [...store.getters[`${pathSubstr}/get${upperCasePath}`]]
           if (data && data.length) {
             if (to.params && Object.keys(to.params).length) {
               store.dispatch(`${pathSubstr}/setCurrent${singlePathObj}`, to.params.id)
-              currentObject = store.getters[`${pathSubstr}/getCurrent${singlePathObj}`]
+              currentObject = { ...store.getters[`${pathSubstr}/getCurrent${singlePathObj}`] }
               if (currentObject && Object.keys(currentObject).length) {
                 next()
               } else redirect('/page404')
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,46 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <q-card v-if="projects && projects.length"
+            flat
+            square
+            class="bg-grey-1">
+      <q-card-section>
+        <div class="text-h5 text-weight-bold text-left text-blue-grey-9">
+          Active investment opportunities
+        </div>
+      </q-card-section>
+      <q-card-section>
+        <standard-grid :data="projects"
+                   :component="projectListCard"/>
+      </q-card-section>
+  </q-card>
   </q-page>
 </template>
 
 <script>
+import moduleFetcher from 'components/mixins/moduleFetcher.js'
+import StandardGrid from 'components/grids/StandardGrid.vue'
+import { defineAsyncComponent, shallowRef } from 'vue'
 export default {
-  name: 'Index'
+  name: 'Index',
+  components: { StandardGrid },
+  mixins: [moduleFetcher],
+  data () {
+    return {
+      projects: [],
+      projectListCard: {}
+    }
+  },
+  created () {
+    this.projectListCard = shallowRef(defineAsyncComponent(
+      () => import('components/cards/ProjectListCard.vue')
+    ))
+    const allProjects = [...this.$store.getters['projects/getProjects']]
+    if (allProjects && allProjects.length) {
+      this.projects.push(allProjects[0])
+      this.projects.push(allProjects[1])
+      this.projects.push(allProjects[2])
+    }
+  }
 }
 </script>
